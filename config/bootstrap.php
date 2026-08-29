a2ab451c62f9086900366926a034ced371a3c2a7
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-08-30 00:36:25 +0200

    Move `vendor/autoload.php` import to `bootstrap.php` for centralized dependency loading.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,3 +2,5 @@
 declare(strict_types=1);
 
 define('ROOT', dirname(__DIR__));
+
+require ROOT . '/vendor/autoload.php';
--- webroot/index.php
+++ webroot/index.php
@@ -26,7 +26,6 @@ if (PHP_SAPI === 'cli-server') {
     }
 }
 
-require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 $envFile = ROOT . '/.env';

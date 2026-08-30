6158ca27b46dc06c55d692a9263333468f53ab7b
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-08-30 14:01:20 +0200

    Define `ROOT` and `TEST_APP` constants in test bootstrap file

--- packages/Core/tests/bootstrap.php
+++ packages/Core/tests/bootstrap.php
@@ -6,3 +6,6 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
 ini_set('intl.default_locale', 'en_US');
+
+define('ROOT', dirname(__DIR__));
+const TEST_APP = ROOT . '/tests/test_app/';

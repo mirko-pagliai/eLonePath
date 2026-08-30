cb2cb4c2739529fd6dd588f1c06b702d17ca55cd
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-08-30 15:25:26 +0200

    Add error templates for 400 and 500 status codes to test app

---
+++ packages/Core/tests/test_app/templates/error/400.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @var bool $debug
+ * @var \Throwable $exception
+ * @var string $message
+ * @var int $status
+ */
+?>
+
+<h1><?= $status ?> <?= htmlspecialchars($message) ?></h1>
+
+<?php if ($debug) : ?>
+    <p><?= htmlspecialchars($exception::class) ?></p>
+<?php endif; ?>
+

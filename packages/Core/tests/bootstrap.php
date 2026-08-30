51f3c0ed7878ca2465a58603b32b0f3c880cdc89
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-08-30 08:47:57 +0200

    Add PHPUnit bootstrap file and lock dependencies for `Core` package

---
+++ packages/Core/tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
+ini_set('intl.default_locale', 'en_US');

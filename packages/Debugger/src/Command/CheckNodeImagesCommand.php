2d820abbe8bcf47e5167dd7cfb489ffb99b5b6dd
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-09-01 17:51:16 +0200

    Refactor: Remove `StoryTesterCommand` and replace it with `CheckNodeImagesCommand` in `Debugger` package, introduced `NodeImagesWalker` for validating node images, and updated `bin/console` accordingly.

---
+++ packages/Debugger/src/Command/CheckNodeImagesCommand.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Elone\Debugger\Command;
+
+use App\Story\Game;
+use Elone\Debugger\NodeImagesWalker;
+use Symfony\Component\Console\Attribute\Argument;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Run with:
+ * ```
+ * $ bin/console debugger:check-node-images full/path/to/story.json
+ * ```
+ */
+#[AsCommand(name: 'debugger:check-node-images')]
+class CheckNodeImagesCommand extends Command
+{
+    public function __invoke(
+        #[Argument('The `story.json` file you want to test')] string $filename,
+        SymfonyStyle $io,
+    ): int {
+        $game = Game::createFromFile(path: $filename);
+
+        $walker = new NodeImagesWalker($game);
+        $errors = $walker();
+
+        if ($errors) {
+            foreach ($errors as $error) {
+                $io->error("<error>$error</error>");
+            }
+
+            return Command::FAILURE;
+        }
+
+        if ($io->isVerbose()) {
+            $io->success('All node images are valid');
+        }
+
+        return Command::SUCCESS;
+    }
+}

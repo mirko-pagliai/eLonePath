fb2bbce28cc2b7450a77bd08d8764fa3ff5c397d
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-09-13 08:45:26 +0200

    Refactor character sheet template: replace `<ul>` with `<div>` for layout flexibility and improve attribute display arrangement.

--- templates/element/character_sheet.php
+++ templates/element/character_sheet.php
@@ -20,27 +20,30 @@ declare(strict_types=1);
     ]) ?>
 
     <div id="collapseExample" class="collapse fs-3 mt-4 mb-2 pb-3" style="border-bottom: 2px solid var(--elone-accent)">
-        <ul class="list-unstyled mb-0 row">
-            <li class="col-6 col-md-3">
+        <div class="d-flex flex-wrap gap-2 justify-content-between">
+            <div>
                 <?= __('Strength: {0}', "<strong>$character->strength</strong>") ?>
-            </li>
-            <li class="col-6 col-md-3">
-                <?= __('Agility: {0}', "<strong>$character->agility</strong>") ?>
-            </li>
-            <li class="col-6 col-md-3">
-                <?= __('Perception: {0}', "<strong>$character->perception</strong>") ?>
-            </li>
-            <li class="col-6 col-md-3">
-                <?= __('Willpower: {0}', "<strong>$character->willpower</strong>") ?>
-            </li>
-        </ul>
+            </div>
 
-        <p class="mb-0 mt-2">
-            <?= __(
+            <div>
+                <?= __('Agility: {0}', "<strong>$character->agility</strong>") ?>
+            </div>
+
+            <div>
+                <?= __('Perception: {0}', "<strong>$character->perception</strong>") ?>
+            </div>
+
+            <div>
+                <?= __('Willpower: {0}', "<strong>$character->willpower</strong>") ?>
+            </div>
+
+            <div>
+                <?= __(
                 'Life points: {0} / {1}',
                 "<strong>$character->lifePoints</strong>",
                 "<strong>$character->maxLifePoints</strong>",
             ) ?>
-        </p>
+            </div>
+        </div>
     </div>
 </aside>
